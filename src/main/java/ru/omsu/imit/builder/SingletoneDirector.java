823886fd4dc7606848fd77b17315bff358d7750c
DanilProtsenko <deadbender27@gmail.com>
2019-11-17 16:53:43 +0600

    added composite example

--- src/main/java/ru/omsu/imit/builder/SingletoneDirector.java
+++ src/main/java/ru/omsu/imit/builder/SingletoneDirector.java
@@ -1,6 +1,9 @@
 package ru.omsu.imit.builder;
 
 public class SingletoneDirector {
+    private SingletoneDirector() {
+    }
+
     public static Computer buildComputer(ComputerBuilder builder) {
         builder.createNewComputer();
         builder.buildCpu();
